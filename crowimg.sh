02c77306fa3530aa711686b69b36186cebbbad6d
Henrique Bissoli Silva <emp.shad@gmail.com>
2018-04-27 01:22:35 -0300

    Update crowimg.sh

--- crowimg.sh
+++ crowimg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-exiting(){ exit 1 }
+exiting(){ exit 1; }
 trap exiting SIGINT # Ctrl+C
 trap exiting SIGQUIT # Terminate
 trap exiting SIGTSTP # Ctrl+Z
-helptxt(){ echo -e "\n\tUsage:\n\tbash $0 image.jpeg\n\tbash $0 image.jpg\n\tbash $0 image.png\n" }
+helptxt(){ echo -e "\n\tUsage:\n\tbash $0 image.jpeg\n\tbash $0 image.jpg\n\tbash $0 image.png\n"; }
 if [[ "$1" ]]; then
 	convert $1 -strip -trim -compress jpeg -quality 100 $1
 fi
