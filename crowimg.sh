87b7b392e2e883f4597e5beca001b24271fd2ca1
Henrique Bissoli Silva <emp.shad@gmail.com>
2018-04-27 00:29:03 -0300

    Update crowimg.sh

--- crowimg.sh
+++ crowimg.sh
@@ -7,3 +7,28 @@ if [ -z optipng ]; then
 	sudo apt-get install optipng -y
 fi
 
+exiting(){
+	exit 1
+}
+trap exiting SIGINT # Ctrl+C
+trap exiting SIGQUIT # Terminate
+trap exiting SIGTSTP # Ctrl+Z
+
+helptxt(){
+	echo -e "\n\t${BOLD}Usage: bash $0 100 image.jpg${NC}"
+	echo -e "\t${BOLD}First param: Image quality${NC}"
+	echo -e "\t${BOLD}Second param: Image${NC}\n"
+}
+
+if [[ "$2" ]]; then
+	imagetype="${2##*.}"
+	if [ imagetype -eq "jpg" ]; then
+		jpegoptim -stpPSm $1 $2
+	fi
+
+	if [ imagetype -eq "png" ]; then
+		optipng -o 7 $2
+	fi
+else
+	helptxt
+fi
